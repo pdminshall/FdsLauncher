bdc3567cf2e9b770bd0c5613d7a99de376a9dbce
Paul Minshall <paulminshall@me.com>
2020-02-21 22:18:23 +0000

    Added collection to store each command line, even if it extends over multiple lines, using the & and / characters.

--- FdsLauncher/Classes/FdsFile.cs
+++ FdsLauncher/Classes/FdsFile.cs
@@ -20,6 +20,8 @@ namespace FdsLauncher
 
         public List<string> FileContents { get; private set; }
 
+        public List<string> CommandLines { get; private set; }
+
         public string ChId { get; private set; }
 
         public string Title { get; private set; } = "";
@@ -60,11 +62,14 @@ namespace FdsLauncher
 
             FileContents = lines.ToList();
 
+            // Parse file lines into separate commands lines
+            ParseCommands();
+
             // TODO: Parse file for parameters
-            foreach (string line in FileContents)
+            foreach (string line in CommandLines)
             {
                 // Look for HEAD, CHID and TITLE
-                if (line.StartsWith("&HEAD"))
+                if (line.StartsWith("HEAD"))
                 {
                     Regex filter = new Regex(@"CHID='[^']*'");
                     Match match = filter.Match(line);
@@ -92,6 +97,68 @@ namespace FdsLauncher
             {
                 IsValidFile = true;
             }
+        }
+
+        /// <summary>
+        /// Method to group file lines into separate commands, using & and / characters as
+        /// start and end points.
+        /// </summary>
+        private void ParseCommands()
+        {
+            // Start state machine
+            string commandLine = "";
+            string state = "";
+            CommandLines = new List<string>();
+
+            foreach (string line in FileContents)
+            {
+                string myLine = line;
+
+                // Remove white space from start and end of line
+                myLine = myLine.Trim();
+
+                // Single line command
+                if (state == "" && myLine.StartsWith("&") && myLine.EndsWith("/"))
+                {
+                    commandLine = myLine.TrimStart('&').TrimEnd('/').Trim();
+                    CommandLines.Add(commandLine);
+                }
+
+                // Detect start of multi-line command
+                if (state == "" && myLine.StartsWith("&") && !myLine.EndsWith("/"))
+                {
+                    // Clear out command line and remove &
+                    commandLine = myLine.TrimStart('&').Trim();
+                    state = "CMD";
+                }
+
+                // Intermediate command line
+                if (state == "CMD" && !myLine.StartsWith("&") && !myLine.EndsWith("/"))
+                {
+                    if (!commandLine.EndsWith(",") && !myLine.StartsWith(",") &&
+                        myLine != "")
+                    {
+                        commandLine += ",";
+                    }
+                    commandLine += myLine;
+                }
+
+                // End of multi-line command
+                if (state == "CMD" && !myLine.StartsWith("&") && myLine.EndsWith("/"))
+                {
+                    if (!commandLine.EndsWith(",") && !myLine.StartsWith(",") &&
+                        !myLine.StartsWith("/"))
+                    {
+                        commandLine += ",";
+                    }
+                    commandLine += myLine.TrimEnd('/').Trim();
+                    CommandLines.Add(commandLine);
+                    state = "";
+                }
+
+            }
+
+            // TODO: Detect mismatching & and /
         }
 
     }
